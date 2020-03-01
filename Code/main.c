4262ca3be9fe32fb0b2163c95f1a72998178ef8d
congee <171240513@smail.nju.edu.cn>
2020-03-01 14:57:27 +0000

    scanner finish

---
+++ Code/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+extern FILE* yyin;
+extern char* yytext;
+#define MY_DEFINE(R) #R
+extern enum TokenType token;
+
+static char* tokens[] = { "ID",
+    "SEMI",
+    "INT",
+    "FLOAT",
+    "COMMA",
+    "RELOP",
+    "ASSIGNOP",
+    "PLUS",
+    "MINUS",
+    "STAR",
+    "DIV",
+    "AND",
+    "OR",
+    "DOT",
+    "NOT",
+    "TYPE",
+    "LP",
+    "RP",
+    "LB",
+    "RB",
+    "LC",
+    "RC",
+    "STRUCT",
+    "RETURN",
+    "IF",
+    "ELSE",
+    "WHILE" };
+
+int main(int argc, char** argv)
+{
+    int ret;
+    if (argc > 1) {
+        if (!(yyin = fopen(argv[1], "r"))) {
+            perror(argv[1]);
+            return 1;
+        }
+    }
+    while (ret = yylex()) {
+        if (ret >= 256) {
+            printf("%s ", tokens[ret - 256]);
+        } else {
+            printf("%d ", ret);
+        }
+        printf("%s\n", yytext);
+    }
+    return 0;
+}
